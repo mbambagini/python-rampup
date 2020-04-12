a6decad12021e1683391924997c5aef92839b3b6
Mario Bambagini <mario.bambagini@gmail.com>
2020-04-12 09:14:00 +0100

    classes+import

---
+++ 08_classes.py
@@ -0,0 +1,28 @@
+class Polygon(object):
+    """Base class"""
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def perimeter(self):
+        return 0
+    def area(self):
+        return 0
+
+class Rectangle(Polygon):
+    """Derived class"""
+    def __init__(self, name, side1 = 10, side2 = 10):
+        super(Rectangle,self).__init__(name)
+        self.side1 = side1
+        self.side2 = side2
+    def perimeter(self):
+        return (self.side1 + self.side2) * 2
+    def area(self):
+        return self.side1 * self.side2
+
+# class example with inheritance and polymorphism
+polygon = Rectangle("my figure", 2, 5)
+print("The name is: " + polygon.getName())
+print("The area is: " + str(polygon.area()))
+print("The perimeter is: " + str(polygon.perimeter()))
+
---
+++ 09_import.py
@@ -0,0 +1,7 @@
+#import filename
+#import filename as packagename
+#from filename import function1, function2, function3
+#from filename import function1 as newfunctionname
+#from filename import *
+#from filename import class1, class2, class3
+
